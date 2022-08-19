8563f37eaaa7895b77317307e35487cd3f561890
Chel-Wal <109593099+Chel-Wal@users.noreply.github.com>
2022-08-19 13:10:09 +0100

    Chages made to all pages, have maaged to create a database, ad patiets can create an account, add a note, uodate the note, delete the note and change their passworrd

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
@@ -7,23 +7,25 @@ db = SQLAlchemy()
 DB_NAME = "database.db"
 
 
+
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SECRET_KEY']="I am the secret key"
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
     db.init_app(app)
 
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(views,url_prefix="/")
+    app.register_blueprint(auth,url_prefix="/")
 
     from .models import User, Note
+    create_db(app)
 
-    create_database(app)
-
-    login_manager = LoginManager()
+    login_manager=LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
@@ -33,8 +35,7 @@ def create_app():
 
     return app
 
-
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
+def create_db(app):
+    if not path.exists('wesite/'+DB_NAME):
         db.create_all(app=app)
-        print('Created Database!')
+        print('database created !')
